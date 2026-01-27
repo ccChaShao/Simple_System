be5b3f1d5f5c614ee90faf30d73c06999c44a29c
charsiewli <805079237@qq.com>
2026-01-27 18:34:55 +0800

    [meshui] 提交；

---
+++ Assets/Sys_ImageFontText/ImageFontData.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MySystem.ImageFont
+{
+    [CreateAssetMenu(fileName = "New ImageFont Data", menuName = "MySystem/UI/ImageFont Data")]
+    public class ImageFontData : ScriptableObject
+    {
+        [Serializable]
+        public class ChatSprioteMapping
+        {
+            public char character;
+            public Sprite sprite;
+        }
+    
+        public List<ChatSprioteMapping> mappings = new ();
+
+        private Dictionary<char, Sprite> m_SpriteDict = new();
+
+        public Sprite GetSprite(char character)
+        {
+            if (m_SpriteDict == null)
+            {
+                m_SpriteDict = new();
+                foreach (var mapping in mappings)
+                {
+                    if (!m_SpriteDict.ContainsKey(mapping.character))
+                    {
+                        m_SpriteDict[mapping.character] = mapping.sprite;
+                    }
+                }
+            }
+
+            m_SpriteDict.TryGetValue(character, out Sprite sprite);
+            return sprite;
+        }
+    }
+}
--- Assets/Sys_ImageFontText/ImageFontText.cs
+++ Assets/Sys_ImageFontText/ImageFontText.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using MySystem.ImageFont;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class ImageFontText : MonoBehaviour
+[AddComponentMenu("MySystem/UI/Image Font Text")]
+public class ImageFontText : MaskableGraphic
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private ImageFontData m_FontData;
+    public ImageFontData fontData
     {
-        
+        get { return m_FontData; }
+        set { m_FontData = value; SetAllDirty();}   // 设置新数据时标记脏数据，触发重建
     }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField, TextArea(3, 10)] private string m_Text;
+    public string text
     {
-        
+        get { return m_Text; }
+        set { m_Text = value; SetAllDirty();}   // 文本变化时标记脏数据，触发重建
+    }
+
+    // 确保使用正确的纹理进行渲染
+    public override Texture mainTexture
+    {
+        get
+        {
+            if (m_FontData == null)
+            {
+                return base.mainTexture;
+            }
+            Sprite sprite = m_FontData.GetSprite('1');
+            if (sprite == null)
+            {
+                return base.mainTexture;
+            }
+            return sprite.texture;
+        }
+    }
+
+    // 辅助方法：向VertexHelper中添加一个四边形（两个三角形）
+    // （字体的网格是左上角为起点，图片的网格是左下角为起点）
+    private void AddQuad(VertexHelper vh, Vector3 bottomLeft, Vector3 bottomRight, Vector3 topLeft, Vector3 topRight, Color color, Vector4 uv)
+    {
+        int startVertexIndex = vh.currentIndexCount;
     }
 }
